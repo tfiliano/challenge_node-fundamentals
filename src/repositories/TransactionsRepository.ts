bd068ccc3079be38c4bd3956b93b826bab15da11
Thiago Figliano <tfiliano@gmail.com>
2020-07-17 16:57:30 -0300

    done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce((balance: Balance, transaction: Transaction) => {
+      balance[transaction.type] += transaction.value;
+
+      if (transaction.type === 'outcome')
+        balance.total = balance.total - transaction.value
+      else
+        balance.total = balance.total + transaction.value
+
+      return balance
+    }, { income: 0, outcome: 0, total: 0 });
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(title: string, value: number, type: 'income' | 'outcome'): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type
+    })
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!["income", "outcome"].includes(type)) {
+      throw new Error('Type must to be income or outcome')
+    }
+
+    if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+
+      if (value > balance.total) {
+        throw new Error('Insufficient funds for this transaction');
+      }
+    }
+
+    const transaction = this.transactionsRepository.create(
+      title,
+      value,
+      type
+    )
+
+    return transaction
   }
 }
 
